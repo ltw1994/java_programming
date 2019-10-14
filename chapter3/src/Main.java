fa6459c6512a6eb13986b43f0d89332ccd8fedf1
임태완 <ltw2001@hanmail.net>
2019-10-14 16:32:26 +0900

    10.14 update

--- chapter3/src/Main.java
+++ chapter3/src/Main.java
@@ -2,12 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double x1, y1, x2, y2, x3, y3, x4, y4;
-        double x, y;
-        double lcm_num ;
-        double a, b, c, d, e, f, P ;
-
-
+        double x1, y1, x2, y2, x3, y3;
+        double a, b ;
+        double result ;
         Scanner scan = new Scanner(System.in);
         x1 = scan.nextDouble();
         y1 = scan.nextDouble();
@@ -15,42 +12,45 @@ public class Main {
         y2 = scan.nextDouble();
         x3 = scan.nextDouble();
         y3 = scan.nextDouble();
-        x4 = scan.nextDouble();
-        y4 = scan.nextDouble();
 
-        a = x1 - x2 ;
-        b = y1 - y2 ;
-        c = x3 - x4 ;
-        d = y3 - y4 ;
-        e = a * y1 + b * x1 ;
-        P =
+        result = (x2-x1) * (y3-y1) - (x3 - x1) * (y2 - y1) ;
+        if (result > 0) System.out.println("left");
+        else if (result < 0) System.out.println("right");
+        else System.out.println("on the line");
 
-        if ((x2 - x1) / (y2 - y1) == (x4 - x3) / (y4 - y3)) {
-            System.out.println("parallel");
+
+//        a = (y2 - y1) / (x2 - x1) ;
+//        b = y2 - a * x2 ;
+//        if(a * x3 + b < y3) {
+//            if (a > 0) {
+//                System.out.println("left");
+//            }
+//            else if(a == 0){
+//                System.out.println("on the line");
+//
+//            }
+//            else {
+//                System.out.println("right");
+//            }
+//        }
+//        else if (a * x3 + b > y3) {
+//            if (a > 0) {
+//                System.out.println("right");
+//            }
+//            else if(a == 0){
+//                System.out.println("on the line");
+//
+//            }
+//            else {
+//                System.out.println("left");
+//            }
+//        }
+//        else {
+//            System.out.println("on the line");
+//        }
+
+
         }
 
-        lcm_num = lcm(a, c) ;
 
-        b *= lcm_num / a ;
-
-        d *= lcm_num / c ;
-
-    }
-
-
-    private static double gcd(double num1, double num2) {
-
-        while (num2 > 0) {
-            double temp = num2;
-            num2 = num1 % num2;
-            num1 = temp;
-        }
-
-        return num1;
-    }
-
-    private static double lcm(double num1, double num2) {
-
-        return (num1 * num2) / gcd(num1, num2);
-    }
 }
